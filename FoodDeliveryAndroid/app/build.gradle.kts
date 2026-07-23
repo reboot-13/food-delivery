52e316e576c78df4df25a98c9f91adb2362929b2
Roman <roma.ermilov.02>
2026-07-23 19:50:41 +0300

    add dependencies to project

--- FoodDeliveryAndroid/app/build.gradle.kts
+++ FoodDeliveryAndroid/app/build.gradle.kts
@@ -1,14 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.android)
+
 }
 
 android {
     namespace = "com.example.fooddeliveryandroid"
-    compileSdk {
-        version = release(37) {
-            minorApiLevel = 1
-        }
+    compileSdk = 36
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
@@ -30,12 +35,11 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
     buildFeatures {
         compose = true
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
@@ -55,4 +59,32 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+//Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.hilt.compiler)
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+// ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+// Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+// Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
+// OkHttp
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+// Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+// DataStore
+    implementation(libs.androidx.datastore.preferences)
+// Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network)
+
 }
